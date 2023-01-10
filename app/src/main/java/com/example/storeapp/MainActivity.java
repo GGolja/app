8bacbcdcffe4975b2db2b6540ed51e793aa23ff1
GGolja <gg8283@student.uni-lj.si>
2023-01-10 22:19:12 +0100

    app1

--- app/src/main/java/com/example/storeapp/MainActivity.java
+++ app/src/main/java/com/example/storeapp/MainActivity.java
@@ -4,11 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView branches;
+    private String url = "https://store-is.azurewebsites.net/api/v1/branch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        branches = (TextView) findViewById(R.id.branches);
     }
+
+    public  void showBranches(View view){
+        if (view != null){
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response){
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object =response.getJSONObject(i);
+                    String name = object.getString("branchName");
+                    String address = object.getString("string");
+                    String mail = object.getString("email");
+                    int num = object.getInt("phoneNumber");
+
+                    data.add(name + " " + address + " " + mail + " " + num);
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+
+                }
+            }
+
+            branches.setText("");
+
+
+            for (String row: data){
+                String currentText = branches.getText().toString();
+                branches.setText(currentText + "\n\n" + row);
+            }
+
+        }
+
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
 }
